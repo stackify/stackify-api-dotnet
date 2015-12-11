b4f14a37e6bf2a96fc86dabf7c908f2edfa0637e
Adam Larsen <adam@collabip.com>
2015-12-11 17:20:22 -0600

    General Clean up and addition of Unit Test - Added unit test for Log4net appender   * Testing handing of Context Stack - Updated .gitignore - Changed Post build events to only run on Release   * Updating the DLL folder only on Release will help to ensure you never accidently check in a debug build. - Updated Fody   * Old version had issies with Roslyn and VS 2015 - Clean up extra copies of DLLs that were checked in

---
+++ Src/StackifyLib/Internal/Logs/ILogClient.cs
@@ -0,0 +1,18 @@
+﻿using StackifyLib.Models;
+using StackifyLib.Utils;
+
+namespace StackifyLib.Internal.Logs
+{
+    public interface ILogClient
+    {
+        bool CanQueue();
+        bool CanSend();
+        bool CanUpload();
+        void Close();
+        bool ErrorShouldBeSent(StackifyError error);
+        AppIdentityInfo GetIdentity();
+        bool IsAuthorized();
+        void PauseUpload(bool isPaused);
+        void QueueMessage(LogMsg msg);
+    }
+}
