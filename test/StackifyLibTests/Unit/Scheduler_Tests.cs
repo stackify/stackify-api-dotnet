21cb79c1223d0aaa1750a3680c281f50e31d082f
jaredcnance <jaredcnance@gmail.com>
2017-08-04 15:02:25 -0500

    allow scheduler to be restarted after it has been stopped
    also fixes a potential null ref in Logger

---
+++ test/StackifyLibTests/Unit/Scheduler_Tests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Threading;
+using StackifyLib.Internal.Scheduling;
+using Xunit;
+
+namespace StackifyLibTests.Unit
+{
+    public class Scheduler_Tests
+    {
+        [Fact]
+        public void Can_Schedule_Task()
+        {
+            // arrange
+            const int expectedExecutionCount = 2;
+            const int intervalMilliseconds = 500;
+            const int sleepInterval = intervalMilliseconds * expectedExecutionCount + intervalMilliseconds / 2;
+
+            var executionCount = 0;
+            void Callback(object state) => executionCount += 1;
+            var scheduler = new Scheduler();
+
+            // act
+            scheduler.Schedule(Callback, TimeSpan.FromMilliseconds(intervalMilliseconds));
+            Thread.Sleep(sleepInterval);
+
+            // assert
+            Assert.Equal(expectedExecutionCount, executionCount);
+        }
+
+        [Fact]
+        public void Can_Pause_Task()
+        {
+            // arrange
+            const int expectedExecutionCount = 1;
+            const int intervalMilliseconds = 500;
+            const int sleepInterval = intervalMilliseconds * expectedExecutionCount + intervalMilliseconds / 2;
+
+            var executionCount = 0;
+            void Callback(object state) => executionCount += 1;
+            var scheduler = new Scheduler();
+
+            // act
+            scheduler.Schedule(Callback, TimeSpan.FromMilliseconds(intervalMilliseconds));
+
+            Thread.Sleep(sleepInterval);
+
+            scheduler.Pause();
+
+            Thread.Sleep(sleepInterval);
+
+            // assert
+            Assert.Equal(expectedExecutionCount, executionCount);
+        }
+
+        [Fact]
+        public void Can_ReSchedule_Using_Same_Handler()
+        {
+            // arrange
+            const int expectedExecutionCount = 1;
+            const int intervalMilliseconds = 500;
+            const int sleepInterval = intervalMilliseconds * expectedExecutionCount + intervalMilliseconds / 2;
+
+            var executionCount = 0;
+            void Callback(object state) => executionCount += 1;
+
+            var scheduler = new Scheduler();
+
+            // act
+            scheduler.Schedule(Callback, TimeSpan.FromMilliseconds(intervalMilliseconds));
+
+            Thread.Sleep(sleepInterval);
+
+            scheduler.Pause();
+
+            scheduler.Schedule(Callback, TimeSpan.FromMilliseconds(intervalMilliseconds));
+
+            Thread.Sleep(sleepInterval);
+
+            // assert
+            Assert.Equal(expectedExecutionCount * 2, executionCount);
+        }
+
+        [Fact]
+        public void Cannot_ReSchedule_Using_Different_Handler()
+        {
+            // arrange
+            const int expectedExecutionCount = 1;
+            const int intervalMilliseconds = 500;
+            const int sleepInterval = intervalMilliseconds * expectedExecutionCount + intervalMilliseconds / 2;
+
+            var executionCount = 0;
+            void Callback1(object state) => executionCount += 1;
+            void Callback2(object state) => Callback1(state);
+
+            var scheduler = new Scheduler();
+
+            // act
+            scheduler.Schedule(Callback1, TimeSpan.FromMilliseconds(intervalMilliseconds));
+
+            Thread.Sleep(sleepInterval);
+
+            scheduler.Pause();
+
+            scheduler.Schedule(Callback2, TimeSpan.FromMilliseconds(intervalMilliseconds));
+
+            Thread.Sleep(sleepInterval);
+
+            // assert
+            Assert.Equal(expectedExecutionCount, executionCount);
+        }
+    }
+}
