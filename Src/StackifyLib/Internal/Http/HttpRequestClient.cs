3caa32ddea34291d937a9c09d1a900cfb76975dd
jaredcnance <jaredcnance@gmail.com>
2017-08-08 10:53:18 -0500

    include library version in web requests

--- Src/StackifyLib/Internal/Http/HttpRequestClient.cs
+++ Src/StackifyLib/Internal/Http/HttpRequestClient.cs
@@ -10,62 +10,79 @@ using System.Text;
 using System.IO;
 using System.Net.Http;
 using StackifyLib.Utils;
+using System.Reflection;
 using HttpClient = System.Net.Http.HttpClient;
 
 namespace StackifyLib.Http
 {
     internal class HttpRequestClient : IHttpRequestClient
     {
+        private static readonly string _userAgent;
+
+        static HttpRequestClient()
+        {
+            var version = typeof(HttpRequestClient).GetTypeInfo().Assembly.GetName().Version;
+            _userAgent = $"Stackify/{version}";
+        }
+
         public async Task<T> PostAsync<T>(string uri, Dictionary<string, string> formData)
         {
             StackifyAPILogger.Log("Sending POST form body");
-            using (var client = new HttpClient())
+            using (var client = GetClient())
             {
                 var data = new FormUrlEncodedContent(formData);
                 var response = await client.PostAsync(uri, data);
 
-                if(response.StatusCode == HttpStatusCode.Unauthorized)
+                if (response.StatusCode == HttpStatusCode.Unauthorized)
                     throw new UnauthorizedAccessException();
 
-                if(response.IsSuccessStatusCode == false)
+                if (response.IsSuccessStatusCode == false)
                     throw new HttpRequestException($"Received {response.StatusCode} response from {uri}");
 
                 var content = await response.Content.ReadAsStringAsync();
-                
+
                 return JsonConvert.DeserializeObject<T>(content);
             }
         }
 
         public async Task<HttpResponseMessage> PostAsync(string uri, object json, AccessTokenResponse token, bool compress = false)
         {
-            using (var client = new HttpClient())
+            using (var client = GetClient())
             {
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(token.TokenType, token.AccessToken);
-                
+
                 var content = JsonConvert.SerializeObject(json, new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Ignore });
-                
+
                 var shouldCompress = compress && content.Length > 5000;
 
-                var request = new HttpRequestMessage(HttpMethod.Post, uri) {
+                var request = new HttpRequestMessage(HttpMethod.Post, uri)
+                {
                     Content = GetContent(content, shouldCompress)
                 };
-                
+
                 var response = await client.SendAsync(request);
 
-                if(response.IsSuccessStatusCode == false)
+                if (response.IsSuccessStatusCode == false)
                     StackifyAPILogger.Log($"Received {response.StatusCode} response from {uri}");
-                    
+
                 return response;
             }
         }
 
+        private HttpClient GetClient()
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("User-Agent", _userAgent);
+            return client;
+        }
+
         private HttpContent GetContent(string content, bool compress)
         {
-            if(compress)
+            if (compress)
             {
                 var bytes = Encoding.UTF8.GetBytes(content);
                 var ms = new MemoryStream();
-                
+
                 using (GZipStream gzip = new GZipStream(ms, CompressionLevel.Fastest, true))
                     gzip.Write(bytes, 0, bytes.Length);
 
