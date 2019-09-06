d3b3af043486a682e15a8c86fe8bf1e76c186773
Kevin Gayo <kagaw3000@live.com>
2019-09-06 22:30:26 +0800

    Added custom layout renderer for capturing httpcontext to support async nlog target

---
+++ Src/NLog.Targets.Stackify/StackifyHttpLayoutRenderer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using NLog.Config;
+using NLog.LayoutRenderers;
+using NLog.Web.LayoutRenderers;
+using StackifyLib.Models;
+using Newtonsoft.Json;
+
+namespace NLog.Targets.Stackify
+{
+    [LayoutRenderer("stackify-http")]
+    public class StackifyHttpLayoutRenderer : AspNetLayoutRendererBase
+    {
+        protected override void DoAppend(StringBuilder builder, LogEventInfo logEvent)
+        {
+            var httpRequest = HttpContextAccessor.HttpContext;
+            if (httpRequest != null)
+            {
+#if NETFULL
+                WebRequestDetail webRequest = new WebRequestDetail();
+                webRequest.Load(httpRequest);
+
+                string result = JsonConvert.SerializeObject(webRequest);
+                builder.Append(result);
+#endif
+            }
+        }
+    }
+}
