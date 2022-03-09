8f17f79682aad0397bc96c777e4087e1626619c8
Michael Mantos <miko.mantos@gmail.com>
2022-03-09 14:37:12 +0800

    DOTNET-105 - Add helper for profiler environment and app name

--- Src/StackifyLib/ProfileTracer.cs
+++ Src/StackifyLib/ProfileTracer.cs
@@ -380,5 +380,15 @@ namespace StackifyLib
         {
 
         }
+
+        [MethodImpl(MethodImplOptions.PreserveSig | MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        public void SetEnvironmentName(string environmentName)
+        {
+        }
+
+        [MethodImpl(MethodImplOptions.PreserveSig | MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        public void SetApplicationName(string applicationName)
+        {
+        }
     }
 }
