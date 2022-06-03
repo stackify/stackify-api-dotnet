c9a75b4e76be174440660e2570b849e1dc310c90
Daniel Gidman <danatcofo@gmail.com>
2022-06-03 13:30:18 -0500

    #129 & DoTNET-117 (#130)
    * #129 DOTNET-117 add configuration option to skip capture of the username
    
    * chore: Updating snk resolution so you can build locally
    
    Co-authored-by: Dan Gidman <dgidman@netreo.com>

--- Src/StackifyLib.CoreLogger/Properties/AssemblyInfo.cs
+++ Src/StackifyLib.CoreLogger/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Stackify")]
 [assembly: AssemblyProduct("StackifyLib.CoreLogger")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyKeyFileAttribute("..\\StackifyLib\\Netreo.snk")]
+//[assembly: AssemblyKeyFile("..\\StackifyLib\\Netreo.snk")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
